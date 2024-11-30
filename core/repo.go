0eb2b77266211fdbf60e962e5c99e004b017fa4a
Dellan <mactunechy@gmail.com>
2024-11-30 12:13:59 +0200

    list todos command working

--- core/repo.go
+++ core/repo.go
@@ -7,6 +7,9 @@ import (
 	"os"
 	"path/filepath"
 	"time"
+
+	"github.com/alexeyco/simpletable"
+	"github.com/mergestat/timediff"
 )
 
 func Save(todo string) error {
@@ -17,7 +20,7 @@ func Save(todo string) error {
 
 	file, err := os.OpenFile(dbfile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		log.Fatalln("Error opening file", err)
+		log.Fatalln("Error opening db file", err)
 	}
 	defer file.Close()
 
@@ -28,6 +31,62 @@ func Save(todo string) error {
 	record := []string{todo, currentTime, "TODO"}
 
 	return writer.Write(record)
+}
+
+func List() {
+	dbfile, err := dbFile()
+	if err != nil {
+		log.Fatalln("Db not found", err)
+	}
+
+	file, err := os.Open(dbfile)
+	if err != nil {
+		log.Fatalln("Error opening db file", err)
+	}
+
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+	if err != nil {
+		log.Fatalln("Error reading records from db file", err)
+	}
+	defer file.Close()
+
+	table := simpletable.New()
+
+	table.Header = &simpletable.Header{
+		Cells: []*simpletable.Cell{
+			{Align: simpletable.AlignLeft, Text: "#"},
+			{Align: simpletable.AlignLeft, Text: "DESCRIPTION"},
+			{Align: simpletable.AlignRight, Text: "CREATED AT"},
+			{Align: simpletable.AlignRight, Text: "STATUS"},
+		},
+	}
+
+	for idx, record := range records {
+		timeAgo := formatTime(record[1])
+
+		r := []*simpletable.Cell{
+			{Align: simpletable.AlignRight, Text: fmt.Sprintf("%d", idx+1)},
+			{Align: simpletable.AlignLeft, Text: record[0]},
+			{Align: simpletable.AlignRight, Text: timeAgo},
+			{Align: simpletable.AlignRight, Text: record[2]},
+		}
+
+		table.Body.Cells = append(table.Body.Cells, r)
+	}
+
+	table.SetStyle(simpletable.StyleCompactLite)
+	fmt.Println(table.String())
+}
+
+// Private methods
+
+func formatTime(t string) string {
+	todoTime, err := time.Parse(time.RFC822, t)
+	if err != nil {
+		return t
+	}
+	return timediff.TimeDiff(todoTime)
 }
 
 func dbFile() (string, error) {
--- go.mod
+++ go.mod
@@ -3,7 +3,11 @@ module github.com/mactunechy/go-todo-cli
 go 1.23.1
 
 require (
+	github.com/alexeyco/simpletable v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/mergestat/timediff v0.0.3 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/spf13/cobra v1.8.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 )
